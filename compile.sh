da34c1b454db4fb59fe830f5159b1d809f87e6d2
Martin Kelm <martinkelm@shrt.ws>
2013-07-31 00:04:49 +0200

    fixed compile script

--- compile.sh
+++ compile.sh
@@ -1,6 +1,8 @@
 #/bin/bash
-java -jar bin/compiler/compiler.jar < cat lib/base/init.js lib/base/audio.js lib/base/game.js \
+cat lib/base/init.js lib/base/audio.js lib/base/game.js \
 lib/display/init.js lib/display/change.js lib/display/window.js \
-lib/display/scenes/menu.js lib/display/scenes/planet.js > tmp.js
+lib/display/scenes/menu.js lib/display/scenes/planet.js > cat.js
+java -jar bin/compiler/compiler.jar --js=cat.js > tmp.js
+rm cat.js
 cat lib/base/HEADER tmp.js > lib/display/mct.js
 rm tmp.js
